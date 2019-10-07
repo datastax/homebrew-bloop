f75fdcba32df00e169ce38b03b1a0e113e05874d
Jacek Lewandowski <jacek.lewandowski@datastax.com>
2019-10-07 09:23:20 +0200

    Initial commit

---
+++ bloop.rb
@@ -0,0 +1,55 @@
+class Bloop < Formula
+  desc "Bloop is a build server to compile, test and run Scala fast"
+  homepage "https://github.com/scalacenter/bloop"
+  version "1.3.3-dse-1"
+  url "https://github.com/jacek-lewandowski/bloop/releases/download/v1.3.3-dse-1/install.py"
+  sha256 "fb64c0dc5177973a547085ff7848e1e903792996ae294695fcbd65d3b07971b8"
+  bottle :unneeded
+
+  depends_on "bash-completion"
+  depends_on "python3"
+  depends_on :java => "1.8+"
+
+  def install
+      mkdir "bin"
+      system "python3", "install.py", "--dest", "bin", "--version", version
+      zsh_completion.install "bin/zsh/_bloop"
+      bash_completion.install "bin/bash/bloop"
+      fish_completion.install "bin/fish/bloop.fish"
+
+      # We need to create these files manually here, because otherwise launchd
+      # will create them with owner set to `root` (see the plist file below).
+      FileUtils.mkdir_p("log/bloop/")
+      FileUtils.touch("log/bloop/bloop.out.log")
+      FileUtils.touch("log/bloop/bloop.err.log")
+
+      prefix.install "bin"
+      prefix.install "log"
+  end
+
+  def plist; <<~EOS
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>#{plist_name}</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>#{bin}/bloop</string>
+        <string>server</string>
+    </array>
+    <key>KeepAlive</key>
+    <true/>
+    <key>StandardOutPath</key>
+    <string>#{prefix}/log/bloop/bloop.out.log</string>
+    <key>StandardErrorPath</key>
+    <string>#{prefix}/log/bloop/bloop.err.log</string>
+</dict>
+</plist>
+          EOS
+      end
+
+  test do
+  end
+end
